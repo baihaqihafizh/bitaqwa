0e51ecaa579049203905eed75966eab1ea3341e5
baihaqihafizh <baihaqihafizhabrorni61@gmail.com>
2025-11-04 14:16:06 +0700

    add dependecis

---
+++ lib/pages/video_kajian.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VideoKajian extends StatelessWidget {
+  const VideoKajian({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
